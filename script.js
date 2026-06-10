455ae632ef26e2d2787e57db614579dc1d6648bc
Rajit Prakash <rajitprakash2007@gmail.com>
2026-06-10 11:25:39 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
+function predict(){
 
+    let cgpa = parseFloat(
+        document.getElementById("cgpa").value
+    );
+
+    let iq = parseFloat(
+        document.getElementById("iq").value
+    );
+
+    let score = (cgpa * 10) + (iq / 10);
+
+    let result = "";
+
+    if(score >= 90){
+        result =
+        "Student is likely to get placed.";
+    }
+    else{
+        result =
+        "Student is unlikely to get placed.";
+    }
+
+    document.getElementById("result")
+        .innerHTML = result;
+}
